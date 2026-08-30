24c438c1d28af022b38df84de82aaa1134f7d008
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-08-30 03:54:03 +0100

    added connection string to program

---
+++ aetherport.api/Models/Tag.cs
@@ -0,0 +1,7 @@
+namespace aetherport.api.Models;
+
+public class Tag
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
--- aetherport.api/Program.cs
+++ aetherport.api/Program.cs
@@ -1,3 +1,6 @@
+using aetherport.api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlite(connectionString);
+});
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
